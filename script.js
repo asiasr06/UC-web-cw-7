d7496ad805369ae7dc732f1e496da81045f9ee86
asiasr06 <84863839+asiasr06@users.noreply.github.com>
2022-09-21 18:21:47 +0300

    last cw

--- script.js
+++ script.js
@@ -10,11 +10,17 @@ let random_grade_div = document.getElementById("random-grade");
 
 
 
+grades.pop();
+grades.push(99);
+grades.forEach((item) => {all_grades_div.innerHTML += `<span>${item}<span>`});
 
+// bonus
+// for (let i = 0; i < grades.length; i++) {
+//   all_grades_div.innerHTML += `<span>${grades[i]}<span>`;
+//   }
 
-
-
-
+let randomIndex = Math.floor(Math.random() * grades.length);
+random_grade_div.innerHTML = `<span>#${randomIndex}: ${grades[randomIndex]}<span>`;
 
 /* الجزء الثالث من التمرين */
 
@@ -27,11 +33,27 @@ let course = {
 
 //Change studentsCount,track property here
 
+course.studentsCount = 40;
+course.track = "web";
 
 
 //Console log here
 
+console.log("Hello my name is Asia, i'm in " + course.track + " at " + course.name+ ".");
 
 
 //Create student object here
 
+let student = {
+  name : "Asia",
+  university : "Kuwait University"
+}
+
+// bonus
+let unicodes = [
+    {track : "web", location : "Kuwait University"}, 
+    {track : "flutter", location : "Kuwait University"}, 
+    {track : "game", location : "Kuwait University"}
+  ]
+
+  unicodes.forEach( (item) => {console.log(item.track)} );
